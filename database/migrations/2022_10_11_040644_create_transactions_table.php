65167e12114b4d346fa70e7214f44651cf3c1fc9
RafaAhdiyaka <rafaahdiyaka@gmail.com>
2022-10-11 11:34:46 +0700

    Table Migration

---
+++ database/migrations/2022_10_11_040644_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->foreignId('category_id');
+            $table->string('keterangan');
+            $table->foreignId('jumlah_pemasukan_id');
+            $table->foreignId('jumlah_pengeluaran_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
